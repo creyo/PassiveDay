e08c58fd748c09fb99198402b7204a185f5d58b3
Ali Raza <alirazaqureshi43@gmail.com>
2023-11-15 18:21:45 +0500

    icons

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -2,8 +2,9 @@
     import logo from '$lib/images/logo.png';
     import facebook from '$lib/images/facebook.png';
     import instagram from '$lib/images/instagram.png';
-    import google from '$lib/images/google.png';
-    import apple from '$lib/images/apple.png';
+    import tiktok from '$lib/images/tiktok.png';
+    import twitter from '$lib/images/twitter.png';
+    import youtube from '$lib/images/youtube.png';
 </script>
 
 <footer>
@@ -43,10 +44,18 @@
     <div class="lower-footer">
       <p>2023 Lookscout. All Rights Reserved</p>
       <div class="social">
-        <img src={facebook} alt="">
-        <img src={apple} alt="">
-        <img src={google} alt="">
-        <img src={instagram} alt="">
+
+        <a href="https://www.facebook.com/passivedaycom" target='_blank'> <img src={facebook} alt="" height="24px" width="24px"></a>
+        <a href="https://twitter.com/passivedaycom" target='_blank'>  <img src={twitter} alt="" height="24px" width="24px"></a>
+        <a href="https://www.tiktok.com/@passivedaycom" target='_blank'>  <img src={tiktok} alt="" height="24px" width="24px"></a>
+        <a href="https://www.youtube.com/@PassiveDay" target='_blank'><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5"
+          fill="white"
+          height="24px" width="24px"
+          viewBox="0 0 24 24">
+          <path
+            d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z" />
+        </svg></a>
+        <a href="https://www.instagram.com/passiveday/" target='_blank'><img src={instagram} alt="" height="24px" width="24px"></a>
       </div>
     </div>
   </div>
