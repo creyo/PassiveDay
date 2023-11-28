09e98ce802bb2e7b91c327366e1b7a018cfec47c
Ali Raza <alirazaqureshi43@gmail.com>
2023-11-28 13:06:57 +0500

    loading

--- src/routes/Spinner.svelte
+++ src/routes/Spinner.svelte
@@ -1,3 +1,12 @@
-<div class="spinner-container">
-    <div class="spinner"></div>
+<script>
+  import logo from '$lib/images/logo.png';
+</script>
+
+
+<div class="loading">
+  <div>
+    <img src={logo} alt="">
+    <p>loading....</p>
   </div>
+
+</div>
